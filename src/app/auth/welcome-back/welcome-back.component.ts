59a4304f6c052e46bc6d38877870286b57a04efb
Krasi Stavrev <stavrev.krasimir@gmail.com>
2024-09-05 14:36:21 +0300

    feat(core/user): extract user as a separate component

--- src/app/auth/welcome-back/welcome-back.component.ts
+++ src/app/auth/welcome-back/welcome-back.component.ts
@@ -4,11 +4,22 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 
+import { UserComponent } from '../../core/user/user.component';
+import { type User } from '../../types/User';
+
 @Component({
   selector: 'app-welcome-back',
   standalone: true,
-  imports: [CardModule, ButtonModule, AvatarModule],
+  imports: [CardModule, ButtonModule, AvatarModule, UserComponent],
   templateUrl: './welcome-back.component.html',
   styleUrl: './welcome-back.component.scss',
 })
-export class WelcomeBackComponent {}
+export class WelcomeBackComponent {
+  user: User = {
+    id: 0,
+    name: 'Krasimir Stavrev',
+    email: 'krasimir@stavrev.dev',
+    title: 'Senior Software Engineer',
+    department: 'Technology',
+  };
+}
