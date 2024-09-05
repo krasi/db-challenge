25dae0a8a370c3dcc70c274f9185f5bdcaef8438
Krasi Stavrev <stavrev.krasimir@gmail.com>
2024-09-05 10:50:05 +0300

    feat: add ngrx store devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import {
   ApplicationConfig,
   importProvidersFrom,
   provideZoneChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +19,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
     provideStore(),
-    provideEffects()
-],
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
