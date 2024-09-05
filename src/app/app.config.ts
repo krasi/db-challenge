38a41eea3de26bbb7ce74108f6c18b09dccaa980
Krasi Stavrev <stavrev.krasimir@gmail.com>
2024-09-05 10:45:04 +0300

    feat: add ngrx store, effects and entity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,11 +8,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
-  ],
+    provideStore(),
+    provideEffects()
+],
 };
